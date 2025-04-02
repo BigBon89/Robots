d2e9ecf741a2d942b222be8719b50c9101391d92
0x110 <Sashik95@yandex.ru>
2025-04-02 09:59:03 +0500

    удалил лишние файлы
    Добавил логирование ошибки при восстановлении окон
    
    Разбил сохранение окон на доп классы
    
    Изменил диалог
    
    убрал лишнее поле
    
    добавил сохранение свернуто ли окно
    
    Перенес сохранение состояний окон в отдельный класс
    
    Добавил сохранение состояний окон
    
    убрал комментарии

---
+++ robots/src/main/java/gui/WindowSaver.java
@@ -0,0 +1,46 @@
+package gui;
+
+import log.Logger;
+
+import javax.swing.*;
+
+public class WindowSaver {
+    private final JDesktopPane desktopPane;
+    private final WindowSettingsStorage storage;
+
+    public WindowSaver(JDesktopPane pane) {
+        this.desktopPane = pane;
+        this.storage = new WindowSettingsStorage(MainApplicationFrame.class);
+    }
+
+    public void saveWindows() {
+        for (JInternalFrame frame : desktopPane.getAllFrames()) {
+            String title = frame.getTitle();
+            WindowSettings settings = new WindowSettings(
+                    frame.getX(), frame.getY(),
+                    frame.getWidth(), frame.getHeight(),
+                    frame.isIcon()
+            );
+            storage.save(title, settings);
+        }
+    }
+
+    public void restoreWindows() {
+        for (JInternalFrame frame : desktopPane.getAllFrames()) {
+            String title = frame.getTitle();
+            WindowSettings defaults = new WindowSettings(
+                    frame.getX(), frame.getY(),
+                    frame.getWidth(), frame.getHeight(),
+                    false
+            );
+            WindowSettings settings = storage.load(title, defaults);
+
+            frame.setBounds(settings.x, settings.y, settings.width, settings.height);
+            try {
+                frame.setIcon(settings.minimized);
+            } catch (Exception e) {
+                Logger.debug("Не удалось свернуть окно '" + frame.getTitle() + "': " + e.getMessage());
+            }
+        }
+    }
+}
---
+++ robots/src/main/java/gui/WindowSettings.java
@@ -0,0 +1,14 @@
+package gui;
+
+public class WindowSettings {
+    public int x, y, width, height;
+    public boolean minimized;
+
+    public WindowSettings(int x, int y, int width, int height, boolean minimized) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.minimized = minimized;
+    }
+}
